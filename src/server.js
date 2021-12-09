91ad606f8902b0f40bed4568f398508e32605bdf
MrDonut <35890108+Space-Kraken@users.noreply.github.com>
2021-12-09 15:03:10 -0600

    Fix errors

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+ 'public'));
+app.use(express.static(__dirname+ '/dist/sbfix-test'));
 app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname+ '/dist/ng-sbfix/index.html'));
+    res.sendFile(path.join(__dirname+ '/dist/sbfix-test/index.html'));
 })
